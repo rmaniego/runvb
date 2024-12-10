dbeb86904539c0e151d3928e5926e176226571ab
rmaniego <rod.maniego23@gmail.com>
2024-12-11 01:10:43 +0800

    Add project to GH

---
+++ Backup/Run/Form1.Designer.vb
@@ -0,0 +1,230 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.player = New System.Windows.Forms.PictureBox
+        Me.Label1 = New System.Windows.Forms.Label
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.Label2 = New System.Windows.Forms.Label
+        Me.Label3 = New System.Windows.Forms.Label
+        Me.Timer2 = New System.Windows.Forms.Timer(Me.components)
+        Me.Label4 = New System.Windows.Forms.Label
+        Me.Timer3 = New System.Windows.Forms.Timer(Me.components)
+        Me.Timer4 = New System.Windows.Forms.Timer(Me.components)
+        Me.Timer5 = New System.Windows.Forms.Timer(Me.components)
+        Me.Label5 = New System.Windows.Forms.Label
+        Me.Timer6 = New System.Windows.Forms.Timer(Me.components)
+        Me.Label6 = New System.Windows.Forms.Label
+        Me.Stp = New System.Windows.Forms.PictureBox
+        Me.Label7 = New System.Windows.Forms.Label
+        Me.Timer7 = New System.Windows.Forms.Timer(Me.components)
+        Me.Timer8 = New System.Windows.Forms.Timer(Me.components)
+        CType(Me.player, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.Stp, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'player
+        '
+        Me.player.BackColor = System.Drawing.Color.Transparent
+        Me.player.Image = CType(resources.GetObject("player.Image"), System.Drawing.Image)
+        Me.player.Location = New System.Drawing.Point(-75, 61)
+        Me.player.Name = "player"
+        Me.player.Size = New System.Drawing.Size(180, 98)
+        Me.player.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.player.TabIndex = 0
+        Me.player.TabStop = False
+        Me.player.Visible = False
+        '
+        'Label1
+        '
+        Me.Label1.BackColor = System.Drawing.Color.Transparent
+        Me.Label1.Font = New System.Drawing.Font("Consolas", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.ForeColor = System.Drawing.Color.LightCyan
+        Me.Label1.Location = New System.Drawing.Point(12, 9)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(608, 24)
+        Me.Label1.TabIndex = 2
+        Me.Label1.Text = "Go! Click Run!! to Start..."
+        Me.Label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        Me.Label1.Visible = False
+        '
+        'Timer1
+        '
+        Me.Timer1.Interval = 200
+        '
+        'Label2
+        '
+        Me.Label2.BackColor = System.Drawing.Color.LightBlue
+        Me.Label2.Font = New System.Drawing.Font("Eras Bold ITC", 21.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.ForeColor = System.Drawing.Color.White
+        Me.Label2.Location = New System.Drawing.Point(300, 159)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(95, 38)
+        Me.Label2.TabIndex = 3
+        Me.Label2.Text = "&Run!!"
+        Me.Label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        Me.Label2.Visible = False
+        '
+        'Label3
+        '
+        Me.Label3.BackColor = System.Drawing.Color.LightGreen
+        Me.Label3.Font = New System.Drawing.Font("Eras Bold ITC", 21.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.ForeColor = System.Drawing.Color.White
+        Me.Label3.Location = New System.Drawing.Point(190, 159)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(104, 38)
+        Me.Label3.TabIndex = 4
+        Me.Label3.Text = "Slow.."
+        Me.Label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        Me.Label3.Visible = False
+        '
+        'Timer2
+        '
+        Me.Timer2.Enabled = True
+        Me.Timer2.Interval = 500
+        '
+        'Label4
+        '
+        Me.Label4.BackColor = System.Drawing.Color.Salmon
+        Me.Label4.Font = New System.Drawing.Font("Eras Bold ITC", 21.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label4.ForeColor = System.Drawing.Color.White
+        Me.Label4.Location = New System.Drawing.Point(571, -29)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(49, 38)
+        Me.Label4.TabIndex = 5
+        Me.Label4.Text = "x"
+        Me.Label4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'Timer3
+        '
+        '
+        'Timer4
+        '
+        '
+        'Timer5
+        '
+        Me.Timer5.Enabled = True
+        '
+        'Label5
+        '
+        Me.Label5.BackColor = System.Drawing.Color.Transparent
+        Me.Label5.Font = New System.Drawing.Font("Consolas", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label5.ForeColor = System.Drawing.Color.Red
+        Me.Label5.Location = New System.Drawing.Point(401, 169)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(219, 24)
+        Me.Label5.TabIndex = 6
+        Me.Label5.Text = "20 sec left!"
+        Me.Label5.TextAlign = System.Drawing.ContentAlignment.MiddleRight
+        Me.Label5.Visible = False
+        '
+        'Timer6
+        '
+        Me.Timer6.Interval = 1000
+        '
+        'Label6
+        '
+        Me.Label6.BackColor = System.Drawing.Color.Transparent
+        Me.Label6.Font = New System.Drawing.Font("Arial Rounded MT Bold", 27.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label6.ForeColor = System.Drawing.Color.LightCyan
+        Me.Label6.Location = New System.Drawing.Point(12, 61)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(597, 57)
+        Me.Label6.TabIndex = 7
+        Me.Label6.Text = "..."
+        Me.Label6.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'Stp
+        '
+        Me.Stp.BackColor = System.Drawing.Color.Transparent
+        Me.Stp.Image = CType(resources.GetObject("Stp.Image"), System.Drawing.Image)
+        Me.Stp.Location = New System.Drawing.Point(528, 138)
+        Me.Stp.Name = "Stp"
+        Me.Stp.Size = New System.Drawing.Size(17, 11)
+        Me.Stp.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize
+        Me.Stp.TabIndex = 8
+        Me.Stp.TabStop = False
+        '
+        'Label7
+        '
+        Me.Label7.BackColor = System.Drawing.Color.LightGreen
+        Me.Label7.Font = New System.Drawing.Font("Eras Bold ITC", 21.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label7.ForeColor = System.Drawing.Color.White
+        Me.Label7.Location = New System.Drawing.Point(624, 111)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(179, 38)
+        Me.Label7.TabIndex = 9
+        Me.Label7.Text = " Restart   >"
+        Me.Label7.TextAlign = System.Drawing.ContentAlignment.MiddleLeft
+        '
+        'Timer7
+        '
+        '
+        'Timer8
+        '
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackgroundImage = CType(resources.GetObject("$this.BackgroundImage"), System.Drawing.Image)
+        Me.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.ClientSize = New System.Drawing.Size(632, 206)
+        Me.Controls.Add(Me.Label7)
+        Me.Controls.Add(Me.Stp)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.player)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None
+        Me.Name = "Form1"
+        CType(Me.player, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.Stp, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents player As System.Windows.Forms.PictureBox
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Timer1 As System.Windows.Forms.Timer
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents Timer2 As System.Windows.Forms.Timer
+    Friend WithEvents Label4 As System.Windows.Forms.Label
+    Friend WithEvents Timer3 As System.Windows.Forms.Timer
+    Friend WithEvents Timer4 As System.Windows.Forms.Timer
+    Friend WithEvents Timer5 As System.Windows.Forms.Timer
+    Friend WithEvents Label5 As System.Windows.Forms.Label
+    Friend WithEvents Timer6 As System.Windows.Forms.Timer
+    Friend WithEvents Label6 As System.Windows.Forms.Label
+    Friend WithEvents Stp As System.Windows.Forms.PictureBox
+    Friend WithEvents Label7 As System.Windows.Forms.Label
+    Friend WithEvents Timer7 As System.Windows.Forms.Timer
+    Friend WithEvents Timer8 As System.Windows.Forms.Timer
+
+End Class
---
+++ Backup/Run/Form1.vb
@@ -0,0 +1,197 @@
+﻿Public Class Form1
+    Dim a As Integer = 2
+    Dim b As Integer = 1
+    Dim c As Integer = 0
+    Dim d As Integer = 10
+    Dim g As Integer = 20
+    Dim h As Integer = 0
+    'player.Left = -187
+
+    Private Sub Timer1_Tick(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Timer1.Tick
+
+        player.Left += a
+        If a > 50 Then
+            a = 50
+        End If
+
+    End Sub
+
+    Private Sub Label2_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Label2.Click
+        Label6.Visible = False
+        player.Visible = True
+        h += 1
+        If c = 1 Then
+            Label1.Text = "Reach the red stump to win..."
+            b = 1
+        ElseIf c = 2 Then
+            Label1.Text = "Running swiftly..."
+            b = 1
+            c = 3
+        End If
+
+        Timer1.Enabled = True
+        a += 2
+    End Sub
+    Private Sub Label2_MouseHover(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Label2.MouseHover
+        Label2.Left = 306
+    End Sub
+    Private Sub Label2_MouseLeave(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Label2.MouseLeave
+        Label2.Left = 300
+    End Sub
+    Private Sub Label3_MouseHover(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Label3.MouseHover
+        Label3.Left = 184
+    End Sub
+    Private Sub Label3_MouseLeave(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Label3.MouseLeave
+        Label3.Left = 190
+    End Sub
+
+    Private Sub Timer2_Tick(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Timer2.Tick
+        a -= 2
+        If a < 0 Then
+            a = 0
+        End If
+    End Sub
+
+    Private Sub Label3_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Label3.Click
+        a -= 2
+        If a < 0 Then
+            a = 0
+        End If
+    End Sub
+
+    Private Sub Label4_MouseHover(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Label4.MouseHover
+        Timer3.Enabled = True
+        Timer4.Enabled = False
+    End Sub
+    Private Sub Label4_MouseLeave(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Label4.MouseLeave
+        Timer4.Enabled = True
+        Timer3.Enabled = False
+    End Sub
+
+    Private Sub Timer3_Tick(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Timer3.Tick
+        If Not (Label4.Top > 0) Then
+            Label4.Top += 5
+        Else
+            Timer3.Enabled = False
+        End If
+    End Sub
+
+    Private Sub Timer4_Tick(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Timer4.Tick
+        If Not (Label4.Top < -28) Then
+            Label4.Top -= 5
+        Else
+            Timer4.Enabled = False
+        End If
+    End Sub
+
+    Private Sub Label4_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Label4.Click
+        If Label4.Top > 0 Then
+            Dispose()
+            Close()
+        End If
+    End Sub
+
+    Private Sub Timer5_Tick(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Timer5.Tick
+        b += 1
+        If b > 0 And b < 15 And (b Mod 2 = 1) Then
+            Label6.Visible = False
+        ElseIf b > 16 And Label1.Visible = True Then
+            Label6.Visible = True
+        ElseIf d = 0 Then
+            Label6.Text = "Start now!!"
+            Label1.Visible = True
+            Timer6.Enabled = True
+            Label2.Visible = True
+            Label3.Visible = True
+            Label5.Visible = True
+            Timer5.Enabled = False
+            b = 1
+            c = 1
+        Else
+            Label6.Visible = True
+            Label6.Text = "Starts in " & d & " ..."
+            d -= 1
+        End If
+
+        If h > 0 And h < 15 And (h Mod 2 = 1) Then
+            h += 1
+            Label6.Visible = False
+        ElseIf h > 16 And Label1.Visible = True Then
+            h += 1
+            Label6.Visible = True
+        End If
+    End Sub
+
+    Private Sub Timer6_Tick(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Timer6.Tick
+        Label5.Text = g & " sec left!!"
+        g -= 1
+
+        If g < 0 And player.Left < 528 Then
+            Label6.Text = "You lost!!"
+            Timer7.Enabled = True
+            Label6.Visible = True
+            Label7.Visible = True
+            Label1.Visible = False
+            Timer6.Enabled = False
+            Label2.Visible = False
+            player.Visible = False
+            Label3.Visible = False
+            Label5.Visible = False
+            Stp.Visible = False
+        ElseIf g < 0 And player.Left > 527 Then
+            Label6.Text = "You won!!"
+            Timer7.Enabled = True
+            Label6.Visible = True
+            Label7.Visible = True
+            Label1.Visible = False
+            Timer6.Enabled = False
+            Label2.Visible = False
+            player.Visible = False
+            Label3.Visible = False
+            Label5.Visible = False
+            Stp.Visible = False
+        End If
+    End Sub
+
+    Private Sub Label7_MouseHover(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Label7.MouseHover
+        Timer7.Enabled = True
+        Timer8.Enabled = False
+    End Sub
+    Private Sub Label7_MouseLeave(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Label7.MouseLeave
+        Timer8.Enabled = True
+        Timer7.Enabled = False
+    End Sub
+
+    Private Sub Timer7_Tick(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Timer7.Tick
+        If Not (Label7.Left < 460) Then
+            Label7.Text = " < Restart"
+            Label7.Left -= 5
+        Else
+            Timer7.Enabled = False
+        End If
+    End Sub
+
+    Private Sub Timer8_Tick(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Timer8.Tick
+        If Not (Label7.Left > 620) Then
+            Label7.Text = " > Restart"
+            Label7.Left += 5
+        Else
+            Timer8.Enabled = False
+        End If
+    End Sub
+
+    Private Sub Label7_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Label7.Click
+        If Label7.Left < 500 Then
+            player.Left = -75
+            Stp.Visible = True
+            Timer8.Enabled = True
+            Timer7.Enabled = False
+            Timer5.Enabled = True
+            a = 2
+            c = 0
+            d = 10
+            g = 20
+            h = 0
+        End If
+    End Sub
+End Class
